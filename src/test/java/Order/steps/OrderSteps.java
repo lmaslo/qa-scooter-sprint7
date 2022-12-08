747990196787d55dd22883c56e17432c54a71dce
User <perepechkinaEV@yandex.ru>
2022-12-08 22:34:57 +0300

    add order test

--- src/test/java/Order/steps/OrderSteps.java
+++ src/test/java/Order/steps/OrderSteps.java
@@ -1,6 +1,7 @@
 package Order.steps;
 
 import Courier.models.CreateCourier;
+import Order.models.CreateOrder;
 import io.restassured.http.ContentType;
 import io.restassured.response.ValidatableResponse;
 
@@ -16,6 +17,16 @@ public class OrderSteps {
                 .baseUri(BASE_URI)
                 .when()
                 .get(ROOT)
+                .then();
+    }
+
+    public ValidatableResponse CreateOrder(CreateOrder order) {
+        return given()
+                .contentType(ContentType.JSON)
+                .baseUri(BASE_URI)
+                .body(order)
+                .when()
+                .post(ROOT)
                 .then();
     }
 
--- src/test/java/Order/tests/CreateOrderTests.java
+++ src/test/java/Order/tests/CreateOrderTests.java
@@ -1,6 +1,7 @@
 package Order.tests;
 
 import Order.models.CreateOrder;
+import Order.steps.OrderSteps;
 import io.qameta.allure.Description;
 import io.qameta.allure.junit4.DisplayName;
 import io.restassured.RestAssured;
@@ -14,27 +15,21 @@ import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.notNullValue;
 
 public class CreateOrderTests {
-    @Before
-    public void setUp() {
-        RestAssured.baseURI = "http://qa-scooter.praktikum-services.ru";
-    }
+    OrderSteps orderSteps = new OrderSteps();
+
 
     @Test
     @DisplayName("Создание заказа")
     public void CreateOrderTests() {
-        ArrayList<String> color  = new ArrayList<>();
+        ArrayList<String> color = new ArrayList<>();
         color.add("BLACK");
 
-        CreateOrder createOrder= new CreateOrder("firstName","lastName", "address", 4, "phone", 5,"2020-06-06","comment", color);
+        CreateOrder createOrder = new CreateOrder("firstName", "lastName", "address", 4, "phone", 5, "2020-06-06", "comment", color);
 
-        given()
-                .contentType(ContentType.JSON)
-                .body(createOrder)
-                .when()
-                .post("/api/v1/orders")
-                .then().log().all()
-                .statusCode(201)
-                .body("track",notNullValue());
+        orderSteps.CreateOrder(createOrder)
+                .log().all()
+                 .statusCode(201)
+                .body("track", notNullValue());
 
 
     }
